09801d336b26c22edc97c08d7fc60581bed8c1de
RajTamang456 <therajtmng@gmail.com>
2021-05-05 19:44:15 +0545

    models

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
@@ -9,7 +10,22 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    if request.method == "POST":
+        my_name = request.POST['name']
+        my_email = request.POST['email']
+        my_subject = request.POST['subject']
+        my_message = request.POST['message']
+        data = Contact.objects.create(
+            name = my_name,
+            email = my_email,
+            subject = my_subject,
+            message = my_message,
+        )
+        data.save()
+        message = {"msg":"Your message is sent.Thank You"}
+        return render(request, 'contact.html', message)
+    return render(request,'contact.html')
+
 
 def portfolio(request):
     return render(request, 'portfolio.html')
